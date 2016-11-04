363fe39cd0cb4dbb79ab0d0a20e9f287e7d295af
Francisco André <fandrefh@gmail.com>
2016-11-04 08:10:28 -0300

    Conexão com o banco de dados

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -7,10 +7,19 @@ module.exports = function(app) {
 
   app.post("/pagamentos/pagamento", function(request, response) {
     let pagamento = request.body;
+
     pagamento.status = 'CRIADO';
     pagamento.data = new Date();
+
     console.log("Processando uma requisição de novo pagamento.");
-    console.log(pagamento);
-    response.send('OK');
+
+    let connection = app.persistencia.connectionFactory();
+    let dao = new app.persistencia.PagamentoDao(connection);
+
+    dao.salva(pagamento, function(err, result) {
+      console.log('Pagamento criado.');;
+      response.json(pagamento);
+    });
+
   });
 }
---
+++ persistencia/connectionFactory.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let mysql = require('mysql');
+
+function createDBConnection() {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'payfast'
+  });
+}
+
+module.exports = function() {
+  return createDBConnection;
+}
